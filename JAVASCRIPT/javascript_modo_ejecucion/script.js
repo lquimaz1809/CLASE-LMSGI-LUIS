fa6c15dae2485cc32e2b5c05b953ca45de888c54
lquimaz1809 <lquimaz1809@g.educaand.es>
2025-02-13 12:48:54 +0100

    2025/02/13_CLASE_LMSGI

--- JAVASCRIPT/javascript_modo_ejecucion/script.js
+++ JAVASCRIPT/javascript_modo_ejecucion/script.js
@@ -1,7 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Selecciona el párrafo y agrega el evento click
-    let titulo2 = document.querySelector("h2");
-    parrafo.addEventListener("click", function() {
-        alert("¡Hola Mundo!");
-    });
-});
+    let titulo2 = document.q
