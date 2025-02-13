dc95b9f0981fe542e53fe20e594a61b668066003
lquimaz1809 <lquimaz1809@g.educaand.es>
2025-02-13 12:49:05 +0100

    M

--- JAVASCRIPT/javascript_modo_ejecucion/script.js
+++ JAVASCRIPT/javascript_modo_ejecucion/script.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Selecciona el párrafo y agrega el evento click
-    let titulo2 = document.q
+    let titulo2 = document.querySelector("h2");
+    parrafo.addEventListener("click", function() {
+        alert("¡Hola Mundo!");
+    });
+});
